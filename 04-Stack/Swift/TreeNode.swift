06353803424caec65b66e2acf09ade32056f6821
Anzeron <Anzeron@163.com>
2022-01-18 11:36:52 +0800

    add Swift TreeNode in Stack

---
+++ 04-Stack/Swift/TreeNode.swift
@@ -0,0 +1,21 @@
+//
+//  TreeNode.swift
+//  04-Stack
+//
+//  Created by Anzeron on 2022/1/18.
+//
+
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+         self.val = val
+         self.left = left
+         self.right = right
+     }
+}
