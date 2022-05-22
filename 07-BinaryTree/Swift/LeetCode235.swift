79781d976592c7092279c2fd8ff34415efdd4c06
Anzeron <Anzeron@163.com>
2022-05-22 21:50:26 +0800

    add Swift LeetCode 235

---
+++ 07-BinaryTree/Swift/LeetCode235.swift
@@ -0,0 +1,50 @@
+//
+//  LeetCode235.swift
+//  07-BinaryTree
+//
+//  Created by Anzeron on 2022/2/22.
+//
+/**
+ 来源：力扣（LeetCode）Easy
+ 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+ 
+ 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+
+ 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+ 示例 1:
+ 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ 输出: 6
+ 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ 
+ 示例 2:
+ 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ 输出: 2
+ 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ */
+
+import Foundation
+
+class LeetCode235 {
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        if root == nil || p == nil || q == nil {
+            return nil
+        }
+        
+        let val = root!.val
+        let val1 = p!.val
+        let val2 = q!.val
+        
+        // p,q在右边
+        if val1 > val && val2 > val {
+            return lowestCommonAncestor(root?.right, p, q)
+        }
+        
+        // p,q在左边
+        if val1 < val && val2 < val {
+            return lowestCommonAncestor(root?.left, p, q)
+        }
+        
+        return root
+    }
+}
