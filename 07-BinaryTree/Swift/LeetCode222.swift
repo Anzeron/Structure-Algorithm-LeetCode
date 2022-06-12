a9ed0da74222b9fc15d5fc018d2cd6f29065457c
Anzeron <Anzeron@163.com>
2022-06-12 21:17:43 +0800

    add Swift LeetCode 222

---
+++ 07-BinaryTree/Swift/LeetCode222.swift
@@ -0,0 +1,38 @@
+//
+//  LeetCode222.swift
+//  07-BinaryTree
+//
+//  Created by Anzeron on 2022/6/12.
+//
+/**
+ 来源：力扣（LeetCode）Medium
+ 链接：https://leetcode.cn/problems/count-complete-tree-nodes
+ 
+ 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+
+ 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+
+ 示例 1：
+ 输入：root = [1,2,3,4,5,6]
+ 输出：6
+ 
+ 示例 2：
+ 输入：root = []
+ 输出：0
+ 
+ 示例 3：
+ 输入：root = [1]
+ 输出：1
+ */
+
+import Foundation
+
+class LeetCode222 {
+    func countNodes(_ root: TreeNode?) -> Int {
+        if root == nil {
+            return 0
+        }
+        
+        return countNodes(root?.left) + countNodes(root?.right) + 1
+    }
+}
